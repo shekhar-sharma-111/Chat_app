26471e16ba31a3fe3163924c2c62c050f3b515a8
Your Name <your.email@example.com>
2024-07-20 12:46:33 +0530

    tailwind css setting up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-neutral">Neutral</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-ghost">Ghost</button>
+<button className="btn btn-active btn-link">Link</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
