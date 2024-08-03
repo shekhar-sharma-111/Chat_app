2c316a99f4bfba0b7ea76011cdd36236fd0cfcd0
Shekhar Sharma <shekharsharma7206@gmail.com>
2024-08-03 12:56:49 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,12 +7,10 @@ const generateToken=(userId,res)=>{
     res.cookie('jwt',token,{
        maxage:1*24*60*60*1000,
        httpOnly:true,
-       secure: process.env.NODE_ENV!=='development',
+       // secure: process.env.NODE_ENV!=='development',
+        secure:'production'
        sameSite:'strict' 
-    // httpOnly: true,
-    // secure: false, // Use false in development for testing with HTTP
-    // maxAge: 24 * 60 * 60 * 1000, // 1 day
-    // sameSite:'strict'
+  
     })
 }
 export default generateToken;
