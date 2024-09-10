d18db68cb78283e5c3a7455502ef2cb63f570c5e
shekhar-sharma-111 <shekharsharma7206@gmail.com>
2024-09-10 20:59:56 +0530

    updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken=(userId,res)=>{
     res.cookie('jwt',token,{
        maxage:1*24*60*60*1000,
        httpOnly:true,
-       sameSite:'strict' 
+       sameSite:'strict' ,
        secure: process.env.NODE_ENV!=='development',
         // secure:'production',
     })
